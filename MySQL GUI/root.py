221ed9bcfaa4fd4a1922f12b42b369ae5ae745e3
Bitan09 <bitansaha07@gmail.com>
2024-05-06 13:22:27 -0700

    Update root.py
    >Added option  to show password
    >Added option to submit password by pressing enter
    >Changed entry box to be selected by default

--- MySQL GUI/root.py
+++ MySQL GUI/root.py
@@ -2,36 +2,49 @@ import mysql.connector as sql
 from tkinter import *
 from tkinter import messagebox
 
-# WINDOW 1
+window = Tk()
+
+passeye_bool = False
+
+def show_password():
+    global passeye_bool
+    passeye_bool = not passeye_bool
+    if passeye_bool == True:
+        pass_show.config(image=open_eye)
+        pass_entry.config(show='')
+    else:
+        pass_show.config(image=close_eye)
+        pass_entry.config(show='*')
 
 def main_window():
-    window.geometry('400x400')
+    window.geometry('1280x720')
     pass_frame.destroy()
 
-def password_submit():
+def password_submit(event=None):
     global con1
     passwd_value = pass_entry.get()
     pass_entry.delete(0,END)
     try:
         con1 = sql.connect(host='localhost',user='root',passwd=passwd_value)
         main_window()
-        messagebox.showinfo(title='Correct Password',message='Password entered by user is correct!\nConnection succesful!')
+        messagebox.showinfo(title='Correct Password',message='Password entered by user is correct!\nSuccesful connection established!')
+        window.unbind('<Return>')
     except:
         messagebox.showerror(title='Password mismatch',message='Password entered by user is wrong!')
 
-
-window = Tk()
-
-show_eye = PhotoImage(file='eyeclose.png')
-
+close_eye = PhotoImage(file='eyeclose.png')
+open_eye = PhotoImage(file='eyeopen.png')
 window.config(background='#000000')
 
 window.geometry('380x50')
 pass_frame =Frame(window,pady=10,bg='#000000')
 pass_lab = Label(pass_frame,text='Enter Password',bg='#000000',fg='#F7F308')
-pass_entry = Entry(pass_frame,width=20,bg='#000000',fg='#ffffff',font=('consolas',14))
-pass_show = Button(pass_frame,image=show_eye,bg='#000000')
+pass_entry = Entry(pass_frame,width=20,bg='#FFFFFF',fg='#000000',font=('consolas',14),show='*')
+pass_show = Button(pass_frame,image=close_eye,bg='#000000',command=show_password,activebackground='#000000')
 pass_submit = Button(pass_frame,text='Submit',command=password_submit,bg='#000000',fg='#00ff00',activebackground='#000000',activeforeground='#00ff00')
+
+pass_entry.focus()
+window.bind('<Return>',password_submit)
 
 pass_lab.grid(row=0,column=0)
 pass_entry.grid(row=0,column=1)
